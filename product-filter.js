588c59897acd38a544d4bbff1d551d4c5a7c3340
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-12-17 18:05:58 +0100

    Added structure for cards including the layout and style settings and iterated over results list using handlebars to display the content in cards

--- product-filter.js
+++ product-filter.js
@@ -1,8 +1,14 @@
 var root = 'http://search.spoonflower.com/searchv2/designs';
 
-$.ajax({
-  url: root,
-  method: 'GET'
-}).then(function(data) {
-  console.log(data);
+$(document).ready(function() {
+  $.ajax({
+    url: root,
+    method: 'GET'
+  }).then(function(data) {
+    console.log(data);
+    var productCardTemplateScript = $("#product-card-template").html();
+    var productCardTemplate = Handlebars.compile(productCardTemplateScript);
+    var compiledHtml = productCardTemplate(data);
+    $('.product-card-placeholder').html(compiledHtml);
+  });
 });
