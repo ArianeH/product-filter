7f34889bf2e8ea23855bde42377e95c6865d9ec0
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-12-21 15:58:41 +0100

    Created second filter to sort by sort style

--- product-filter.js
+++ product-filter.js
@@ -1,27 +1,37 @@
 var root = 'http://search.spoonflower.com/searchv2/';
 
 $(document).ready(function() {
-  requestProductsByTag('designs');
+  requestProductsByAttribute('designs');
 });
 
 function getProductsWithTag() {
-  var inputTerm = document.getElementById("input-field").value;
-  requestProductsByTag(getProductsWithTerms(inputTerm));
+  var inputTerm = document.getElementById("tag-input-field").value;
+  requestProductsByAttribute(getProductsWithTerms(inputTerm));
+}
+
+function getProductsWithSortStyle() {
+  var sortInputTerm = $("#sort-selection option:selected").text().toLowerCase();
+  console.log(sortInputTerm)
+  requestProductsByAttribute(getProductsBySort(sortInputTerm));
 }
 
 function getProductsWithTerms(term) {
  return 'designs?q=' + term
 }
 
-function requestProductsByTag(path) {
+function getProductsBySort(sortStyle) {
+  return 'designs?sort=' + sortStyle;
+}
+
+function requestProductsByAttribute(path) {
   $.ajax({
     url: root + path,
     method: 'GET'
   }).then(function(data) {
-    console.log(data);
     var productCardTemplateScript = $("#product-card-template").html();
     var productCardTemplate = Handlebars.compile(productCardTemplateScript);
     var compiledHtml = productCardTemplate(data);
     $('.product-card-placeholder').html(compiledHtml);
   });
 }
+
